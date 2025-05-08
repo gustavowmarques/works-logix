23f141794fad1c0e4740c0f038f0e2ef0fa10048
gustavowmarques <gustavowmarques@gmail.com>
2025-05-08 19:41:09 +0100

    user table added to database

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class WorkOrder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +10,16 @@ class WorkOrder(db.Model):
     created_by = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(20), default='Open')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  # e.g., "admin", "manager", "contractor"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
