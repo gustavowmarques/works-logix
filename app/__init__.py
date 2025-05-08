253f1349c654b7945c170825993c71b31c68500f
gustavowmarques <gustavowmarques@gmail.com>
2025-05-08 19:58:29 +0100

    select client and select contractor dropdown buttons added to new work order form

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     with app.app_context():
-        from . import routes, models  # Import routes and models
+        from . import routes, models, auth  # make sure auth.py exists
         db.create_all()
 
     return app
---
+++ app/auth.py
@@ -0,0 +1,50 @@
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app as app
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form['role']
+
+        if User.query.filter_by(email=email).first():
+            flash('Email already registered.')
+            return redirect(url_for('register'))
+
+        user = User(username=username, email=email, role=role)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful. You can now log in.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Logged in successfully.')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid email or password.')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('login'))
